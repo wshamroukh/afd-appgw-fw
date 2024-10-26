ffd6fd8477d193a4aec441bac06d5f276873d534
wshamroukh <wshamroukh@gmail.com>
2024-10-26 06:14:26 +0400

    vnet integration

--- appgw-fw/appgw-fw.sh
+++ appgw-fw/appgw-fw.sh
@@ -11,6 +11,8 @@ spoke1_appgw_subnet_name=appgw
 spoke1_appgw_subnet_address=10.11.0.0/24
 spoke1_pe_subnet_name=pe
 spoke1_pe_subnet_address=10.11.1.0/24
+spoke1_appsvc_subnet_name=appsvc
+spoke1_appsvc_subnet_address=10.11.2.0/24
 
 spoke1_appgw_name=appgw
 spoke1_app_svc_name=waddahApp-$RANDOM
@@ -27,6 +29,7 @@ az network vnet create -g $rg -n $hub_vnet_name -l $location --address-prefixes 
 echo -e "\e[1;36mCreating $spoke1_vnet_name VNet...\e[0m"
 az network vnet create -g $rg -n $spoke1_vnet_name -l $location --address-prefixes $spoke1_vnet_address --subnet-name $spoke1_appgw_subnet_name --subnet-prefixes $spoke1_appgw_subnet_address -o none
 az network vnet subnet create -g $rg -n $spoke1_pe_subnet_name --address-prefixes $spoke1_pe_subnet_address --vnet-name $spoke1_vnet_name -o none
+az network vnet subnet create -g $rg -n $spoke1_appsvc_subnet_name --address-prefixes $spoke1_appsvc_subnet_address --vnet-name $spoke1_vnet_name -o none
 
 # VNet Peering between hub1 and spoke1
 echo -e "\e[1;36mCreating VNet peerring between $hub_vnet_name and $spoke1_vnet_name...\e[0m"
@@ -45,6 +48,10 @@ echo -e "\e[1;36mCreating Service Endpoint for $spoke1_app_svc_name App Service.
 az network private-endpoint create -g $rg -n $spoke1_app_svc_name-pe --nic-name $spoke1_app_svc_name-pe-nic --vnet-name $spoke1_vnet_name --subnet $spoke1_pe_subnet_name --private-connection-resource-id $appid --group-id sites --connection-name $spoke1_app_svc_name-connection -l $location -o none
 az network private-endpoint show -g $rg -n $spoke1_app_svc_name-pe --query customDnsConfigs[0].fqdn -o tsv
 az network private-endpoint show -g $rg -n $spoke1_app_svc_name-pe --query customDnsConfigs[0].ipAddresses -o tsv
+
+# app service vnet integration:
+echo -e "\e[1;36mEnable VNet integration on $spoke1_app_svc_name App Service...\e[0m"
+az webapp vnet-integration add -g $rg -n $spoke1_app_svc_name --vnet $spoke1_vnet_name --subnet $spoke1_appsvc_subnet_name -o none
 
 # configure private dns
 echo -e "\e[1;36mCreating Private DNS Zone for $spoke1_app_svc_name App Service...\e[0m"
@@ -68,7 +75,7 @@ az extension update -n azure-firewall
 az network firewall policy create -g $rg -n $hub_vnet_name-fw-policy -l $location -o none
 az network firewall policy rule-collection-group create -g $rg -n $hub_vnet_name-RuleCollectionGroup --policy-name $hub_vnet_name-fw-policy --priority 100 -o none
 az network firewall policy rule-collection-group collection add-filter-collection -g $rg -n $hub_vnet_name-NetworkRuleCollection --policy-name $hub_vnet_name-fw-policy --rcg-name $hub_vnet_name-RuleCollectionGroup --action Allow --rule-name appgw-to-appsv-pe-traffic --collection-priority 100 --rule-type NetworkRule --source-addresses $spoke1_appgw_subnet_address --ip-protocols any --destination-addresses $spoke1_pe_subnet_address --destination-ports '*' -o none
-az network firewall policy rule-collection-group collection rule add -g $rg -n appsvc-pe-to-appgw-traffic --policy-name $hub_vnet_name-fw-policy --rule-collection-group-name $hub_vnet_name-RuleCollectionGroup  --collection-name $hub_vnet_name-NetworkRuleCollection --rule-type NetworkRule --dest-addr $spoke1_appgw_subnet_address --destination-ports '*' --ip-protocols any --source-addresses $spoke1_pe_subnet_address -o none
+az network firewall policy rule-collection-group collection rule add -g $rg -n appsvc-vnetintegration-to-appgw-traffic --policy-name $hub_vnet_name-fw-policy --rule-collection-group-name $hub_vnet_name-RuleCollectionGroup  --collection-name $hub_vnet_name-NetworkRuleCollection --rule-type NetworkRule --dest-addr $spoke1_appgw_subnet_address --destination-ports '*' --ip-protocols any --source-addresses $spoke1_appsvc_subnet_address -o none
 
 # hub1 azure firewall
 echo -e "\e[1;36mCreating $hub_vnet_name-fw Azure Firewall....\e[0m"
@@ -92,11 +99,11 @@ az network route-table create -g $rg -n $spoke1_appgw_name -l $location --disabl
 az network route-table route create -g $rg -n to-$spoke1_app_svc_name-pe --address-prefix $spoke1_pe_subnet_address --next-hop-type VirtualAppliance --route-table-name $spoke1_appgw_name --next-hop-ip-address $hub1_fw_private_ip -o none
 az network vnet subnet update -g $rg -n $spoke1_appgw_subnet_name --vnet-name $spoke1_vnet_name --route-table $spoke1_appgw_name -o none
 
-# App Service Private Endpoint UDR
+# App Service VNet Integration Subnet UDR
 echo -e "\e[1;36mCreating $spoke1_appgw_name UDR....\e[0m"
-az network route-table create -g $rg -n $spoke1_pe_subnet_name -l $location --disable-bgp-route-propagation false -o none
-az network route-table route create -g $rg -n default --address-prefix "0.0.0.0/0" --next-hop-type VirtualAppliance --route-table-name $spoke1_pe_subnet_name --next-hop-ip-address $hub1_fw_private_ip -o none
-az network route-table route create -g $rg -n to-$spoke1_appgw_name --address-prefix $spoke1_appgw_subnet_address --next-hop-type VirtualAppliance --route-table-name $spoke1_pe_subnet_name --next-hop-ip-address $hub1_fw_private_ip -o none
-az network vnet subnet update -g $rg -n $spoke1_pe_subnet_name --vnet-name $spoke1_vnet_name --route-table $spoke1_pe_subnet_name -o none
+az network route-table create -g $rg -n $spoke1_appsvc_subnet_name -l $location --disable-bgp-route-propagation false -o none
+az network route-table route create -g $rg -n default --address-prefix "0.0.0.0/0" --next-hop-type VirtualAppliance --route-table-name $spoke1_appsvc_subnet_name --next-hop-ip-address $hub1_fw_private_ip -o none
+az network route-table route create -g $rg -n to-$spoke1_appgw_name --address-prefix $spoke1_appgw_subnet_address --next-hop-type VirtualAppliance --route-table-name $spoke1_appsvc_subnet_name --next-hop-ip-address $hub1_fw_private_ip -o none
+az network vnet subnet update -g $rg -n $spoke1_appsvc_subnet_name --vnet-name $spoke1_vnet_name --route-table $spoke1_appsvc_subnet_name -o none
 
 echo "Try now to access the website through application gateway after routing the traffic to azure firewall: http://$appgwpip"
